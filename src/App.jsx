a81118b7e11c4d12764dfd4fc0f2d84a5ce1d2cb
Mahmoud Emad Eldin <25500531+iMahmoudEmad@users.noreply.github.com>
2020-10-16 22:17:27 +0200

    Added load validation before dropdown rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
-import axios from 'axios';
 import Dropdown from './components/Dropdown';
-import { getApiUrls } from './services/apis.service';
+import { getAllCountries } from './services/countries.service';
 
 const App = () => {
 	const [countries, setCountries] = useState([]);
 	const [load, setLoad] = useState(false);
 	const [error, setError] = useState('');
-	
-	const getAllCountries = async () => {
-		await axios.get(getApiUrls().getAllCountries).then(res => {
-			setCountries([...res.data.data]);
-			setLoad(true);
-		}).catch(err => {
-			setLoad(false);
-			setError(err.message);
-		});
-	}
+
 
 	useEffect(()=> {
-		getAllCountries();
+		getAllCountries()
+			.then(res => {
+				setCountries([...res.data.data]);
+				setLoad(true);
+			}).catch(err => {
+				setLoad(false);
+				setError(err.message);
+			});
 	}, [])
 
 	return (
